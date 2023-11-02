9a76e3820fccfa78cc3bd668c27f04ffa6fbe137
Miroslav <miroslavsabev11@gmail.com>
2023-11-03 00:19:20 +0200

    first commit

---
+++ src/appwriteConfig.ts
@@ -0,0 +1,13 @@
+import { Client, Databases } from "appwrite";
+
+const APPWRITE_ENDPOINT = import.meta.env.VITE_APP_APPWRITE_ENDPOINT;
+export const APPWRITE_PROJECT_ID = import.meta.env.VITE_APP_APPWRITE_PROJECT_ID;
+export const APPWRITE_DATABASE_PROD_ID = import.meta.env.VITE_APP_APPWRITE_DATABASE_PROD_ID;
+
+const client = new Client();
+
+client.setEndpoint(APPWRITE_ENDPOINT).setProject(APPWRITE_PROJECT_ID);
+
+export const databases = new Databases(client);
+
+export default client;
